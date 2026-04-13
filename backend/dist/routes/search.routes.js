c3c1fcf74a17142d9414ad3afdb70240a133b012
infotechsrealmdarshan <infotechsrealm.darshan@gmail.com>
2026-04-13 17:08:57 +0530

    Delete backend directory

--- backend/dist/routes/search.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const search_controller_1 = require("../controllers/search.controller");
-const router = (0, express_1.Router)();
-router.post('/search', search_controller_1.searchController.search);
-router.get('/trident-auth-check', search_controller_1.searchController.tridentAuthCheck);
-exports.default = router;
